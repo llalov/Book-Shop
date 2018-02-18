38b212845a8ebf6bcf995a4fde339a6175a57fb0
Lachezar Lalov <luchoazz@abv.bg>
2018-02-18 19:16:00 +0200

    initial commit

---
+++ BookShop/BookShop.Data/BookShopDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace BookShop.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class BookShopDbContext : DbContext
+    {
+        public BookShopDbContext(DbContextOptions<BookShopDbContext> options)
+            :base(options)
+        {
+        }
+    }
+}
